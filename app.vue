ccca558aa8711335c23616e99ee3130d85077b57
thomasluizon <72260917+thomasluizon@users.noreply.github.com>
2024-07-27 14:59:32 -0300

    feat: added page transition

--- app.vue
+++ app.vue
@@ -11,3 +11,15 @@ useHead({
 	title: `${t('portfolio')} - Thomas Luizon`,
 })
 </script>
+
+<style>
+.rotate-enter-active,
+.rotate-leave-active {
+	transition: all 0.4s;
+}
+.rotate-enter-from,
+.rotate-leave-to {
+	opacity: 0;
+	transform: rotate3d(1, 1, 1, 15deg);
+}
+</style>
